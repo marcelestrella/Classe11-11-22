4c4b3d79bface25af215deae1cc389bc8a1f3c1e
marcelestrella <marcel.estrella39@gmail.com>
2022-11-11 10:36:35 +0100

    Ejercicios tipos compuestos

---
+++ EJERCICIO2.py
@@ -0,0 +1,23 @@
+#1- Crea un programa que mostri una llista de les coses que tens en la ment.
+skins=["ion", "blastX", "oni", "prime"]
+
+print (skins)
+
+#2- Tupla amb els teus videojocs favorits
+juegos=("Valorant", "CSGO", "Fortnite", "COD")
+
+print (juegos[0])
+print (juegos)
+
+#3- Diccionari amb les teves dades:
+persona={
+    "Nom": "Marcel"
+    "Edat": "18 anys"
+    "Esport preferit": "Hoquei"
+    "Enemic mortal": "Senador Palpatine"
+
+}
+
+#4- Set d’habilitats del teu personatge favorit del LOL(o altre videojoc)
+Raze=set("Balas de pintura", "Fardos explosivos", "Bot bomba", "Cierratelones")
+
